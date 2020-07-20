ca815e801207014c785f2ac775d44c1f33d4cf0e
Bruno Alves <alves.bruno945@gmail.com>
2020-07-20 16:28:51 -0300

    People Motivation
    Let's help someone

---
+++ PeopleMotivation/Controller/FrasesController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  PeopleMotivation
+//
+//  Created by Bruno Silva on 11/06/20.
+//  Copyright © 2020 Bruno Silva. All rights reserved.
+//
+
+import UIKit
+
+class FrasesViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
