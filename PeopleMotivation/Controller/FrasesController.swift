d25ae26dd62a6fce1ae9f13e04932a9221703c30
Bruno Alves <alves.bruno945@gmail.com>
2020-07-25 19:05:52 -0300

    New Version

--- PeopleMotivation/Controller/FrasesController.swift
+++ PeopleMotivation/Controller/FrasesController.swift
@@ -10,8 +10,25 @@ import UIKit
 
 class FrasesViewController: UIViewController {
     
+    @IBOutlet weak var txtFrase: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func mudarFrase(_ sender: Any) {
+        mudandoFrase()
+    }
+    
+    func mudandoFrase() {
+        let lista = ["Voce é incrivel!"
+        , "Nunca desista dos seus sonhos!"
+        , "No tempo certo, tudo dará certo"
+        , "Se jogue no que te faz sorrir."
+        , "Amanha é um novo dia!"
+        , "Não tem hora melhor do que o agora!"]
+        guard let random = lista.randomElement() else {return}
+        txtFrase.text = random
     }
 }
 
